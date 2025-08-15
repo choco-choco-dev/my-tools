bd12260d57ee70226348d8d71f4ef08379ef9411
choco-choco-dev <206303345+choco-choco-dev@users.noreply.github.com>
2025-08-15 14:43:09 +0900

    構成変更

---
+++ js/common.js
@@ -0,0 +1,22 @@
+/** ヘッダー選択  */
+
+const diva = document.getElementsByTagName("div");
+
+const header = document.getElementById("header");
+document.getElementById("header-div").addEventListener("click", click());
+
+function click() {
+  console.log("クリック");
+}
+// document.addEventListener("DOMContentLoaded", () => {
+//   /** ヘッダー選択  */
+
+//   const diva = document.getElementsByTagName("div");
+
+//   const header = document.getElementById("#header");
+//   document.getElementById("#header-div").addEventListener("click", click());
+
+//   function click() {
+//     console.log("クリック");
+//   }
+// });
